491870cda0d23f566915c37d46395676fe945d78
PunishedJoe <PunishedJoeseph@proton.me>
2026-04-11 18:37:11 +1000

    kill me

--- Content.Shared/Projectiles/BarricadeBlockSystem.cs
+++ Content.Shared/Projectiles/BarricadeBlockSystem.cs
@@ -1,81 +1,20 @@
-//using System.Numerics;
-//using Content.Shared._RMC14.Weapons.Ranged.Prediction;
-//using Content.Shared.Camera;
-//using Content.Shared.Damage;
-//using Content.Shared.Damage.Components;
-//using Content.Shared.Damage.Systems;
-//using Content.Shared.Database;
-//using Content.Shared.DoAfter;
-//using Content.Shared.Effects;
-//using Content.Shared.Hands.EntitySystems;
-//using Content.Shared.Interaction;
-//using Content.Shared.Mobs.Components;
-//using Content.Shared.Weapons.Ranged.Components;
-//using Content.Shared.Weapons.Ranged.Systems;
-//using Robust.Shared.Audio.Systems;
-//using Robust.Shared.Map;
-//using Robust.Shared.Network;
-//using Robust.Shared.Physics;
-//using Robust.Shared.Physics.Components;
-//using Robust.Shared.Physics.Dynamics;
 //using Robust.Shared.Physics.Events;
-//using Robust.Shared.Physics.Systems;
-//using Robust.Shared.Player;
-//using Robust.Shared.Serialization;
-//using Robust.Shared.Utility;
-//using Robust.Shared.Threading;
-//using System.Collections.Concurrent;
-//using Robust.Shared.Timing;
-//using Content.Shared._Mono;
-//using Content.Shared.Tag;
 //using Content.Shared.BarricadeBlock; // BF14
 //using Robust.Shared.Random; // BF14
 //
 //namespace Content.Shared.Projectiles;
 //
-//public abstract partial class SharedProjectileSystem : EntitySystem
+//public abstract partial class BarricadeBlockSystem : EntitySystem
 //{
-//    public const string ProjectileFixture = "projectile";
 //
-//    [Dependency] private readonly INetManager _netManager = default!;
-//    [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
-//    [Dependency] private readonly SharedAudioSystem _audio = default!;
-//    [Dependency] private readonly SharedColorFlashEffectSystem _color = default!;
-//    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-//    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
-//    [Dependency] private readonly SharedGunSystem _guns = default!;
-//    [Dependency] private readonly SharedCameraRecoilSystem _sharedCameraRecoil = default!;
-//    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
-//    [Dependency] private readonly SharedTransformSystem _transform = default!;
-//    [Dependency] private readonly TagSystem _tag = default!;
-//    [Dependency] private readonly IParallelManager _parallel = default!;
-//    [Dependency] private readonly IGameTiming _gameTiming = default!;
-//    [Dependency] private readonly INetManager _net = default!;
 //    [Dependency] private readonly IRobustRandom _random = default!; // BF14
-//
-//    // Cache of projectiles waiting for collision checks
-//    private readonly ConcurrentQueue<(EntityUid Uid, ProjectileComponent Component, EntityUid Target)> _pendingCollisionChecks = new();
-//    private readonly HashSet<EntityUid> _processedProjectiles = new();
-//    private const int MinProjectilesForParallel = 8;
-//    private const int ProjectileBatchSize = 16;
-//    private TimeSpan _lastBatchProcess;
-//    private readonly TimeSpan _processingInterval = TimeSpan.FromMilliseconds(16); // ~60Hz
+//    [Dependency] private readonly SharedTransformSystem _transform = default!;
 //
 //    public override void Initialize()
 //    {
 //        base.Initialize();
 //
-//        SubscribeLocalEvent<ProjectileComponent, StartCollideEvent>(OnStartCollide);
 //        SubscribeLocalEvent<ProjectileComponent, PreventCollideEvent>(PreventCollision);
-//
-//        SubscribeLocalEvent<EmbeddedContainerComponent, EntityTerminatingEvent>(OnEmbeddableTermination);
-//        // Subscribe to initialize the origin grid on ProjectileGridPhaseComponent
-//        SubscribeLocalEvent<ProjectileGridPhaseComponent, ComponentStartup>(OnProjectileGridPhaseStartup);
-//        // Subscribe to ensure MetaDataComponent on projectile entities for networking
-//        SubscribeLocalEvent<ProjectileComponent, ComponentStartup>(OnProjectileMetaStartup);
-//
-//        // Mono
-//        SubscribeLocalEvent<ProjectileComponent, TileFrictionEvent>(OnTileFriction);
 //    }
 //
 //    //ported from civ14
